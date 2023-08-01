569f8e77e24201e9f2e9b8d8c088bdcf1f5d54a7
YGXXD <20191120197@mail.ynu.edu.cn>
2023-08-02 00:13:49 +0800

    MacWindow

--- XXdEngine/Platform/Mac/MacWindow.hpp
+++ XXdEngine/Platform/Mac/MacWindow.hpp
@@ -1,5 +1,24 @@
 #ifndef MAC_WINDOW_HPP
 #define MAC_WINDOW_HPP
 
+#include "Core/Window.hpp"
+
+namespace xxd
+{
+class MacWindow : public Window
+{
+public:
+	virtual void OnUpdate() override;
+
+    virtual uint32_t GetWidth() const override;
+	virtual uint32_t GetHeight() const override;
+
+	virtual void SetEventCallback(void(* callback)(Event&)) override;
+	virtual void SetVSync(bool enabled) override;
+	virtual bool IsVSync() const override;
+
+  	virtual void *GetNativeWindow() const override;
+} ;
+}
 
 #endif
