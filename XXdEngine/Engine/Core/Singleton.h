864a1e7c39f0201ba773df766040fc15e4493c69
chenqiudu <chenqiudu@kuaishou.com>
2023-08-07 16:37:07 +0800

    singleton

--- XXdEngine/Engine/Core/Singleton.h
+++ XXdEngine/Engine/Core/Singleton.h
@@ -46,6 +46,7 @@ className(); \
 className(const className&) = delete; \
 className(className&&) = delete; \
 ~className(); \
-className& operator=(const className&) = delete;
+className& operator=(const className&) = delete; \
+className& operator=(className&&) = delete;
 
 #endif
