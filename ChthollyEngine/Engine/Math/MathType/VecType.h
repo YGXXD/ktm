8df56dcc29df6e7c1f7f03450924f8c16fcf8b04
chenqiudu <chenqiudu@kuaishou.com>
2023-09-13 15:56:32 +0800

    math type

--- ChthollyEngine/Engine/Math/MathType/VecType.h
+++ ChthollyEngine/Engine/Math/MathType/VecType.h
@@ -18,13 +18,6 @@ struct vec<N, T> : ktl::SingleExtends_t<ktl::TemplateList<IArray, IVecData, IVec
 };
 
 template<int N>
-using svec = vec<N, int>;
-using svec1 = svec<1>;
-using svec2 = svec<2>;
-using svec3 = svec<3>;
-using svec4 = svec<4>;
-
-template<int N>
 using fvec = vec<N, float>;
 using fvec1 = fvec<1>;
 using fvec2 = fvec<2>;
@@ -32,11 +25,11 @@ using fvec3 = fvec<3>;
 using fvec4 = fvec<4>;
 
 template<int N>
-using dvec = vec<N, double>;
-using dvec1 = dvec<1>;
-using dvec2 = dvec<2>;
-using dvec3 = dvec<3>;
-using dvec4 = dvec<4>;
+using svec = vec<N, int>;
+using svec1 = svec<1>;
+using svec2 = svec<2>;
+using svec3 = svec<3>;
+using svec4 = svec<4>;
 
 template<int N>
 using uvec = vec<N, unsigned int>;
@@ -44,6 +37,13 @@ using uvec1 = uvec<1>;
 using uvec2 = uvec<2>;
 using uvec3 = uvec<3>;
 using uvec4 = uvec<4>;
+
+template<int N>
+using dvec = vec<N, double>;
+using dvec1 = dvec<1>;
+using dvec2 = dvec<2>;
+using dvec3 = dvec<3>;
+using dvec4 = dvec<4>;
 
 }
 
