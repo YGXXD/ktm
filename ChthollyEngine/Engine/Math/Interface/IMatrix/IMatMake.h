65a84988346b57ff758b32713865790fa770b604
chenqiudu <chenqiudu@kuaishou.com>
2023-09-28 13:49:06 +0800

    make mat from diag

--- ChthollyEngine/Engine/Math/Interface/IMatrix/IMatMake.h
+++ ChthollyEngine/Engine/Math/Interface/IMatrix/IMatMake.h
@@ -23,6 +23,21 @@ private:
     }
 };
 
+template<class Father, typename T>
+struct IMatMake<Father, mat<2, 2, T>> : Father
+{
+    static CHTHOLLY_INLINE mat<2, 2, T> from_row(const vec<2, T>& row0, const vec<2, T>& row1) noexcept
+    {
+        return mat<2, 2, T>({ { reinterpret_cast<const T*>(&row0)[0], reinterpret_cast<const T*>(&row1)[0] },
+                              { reinterpret_cast<const T*>(&row0)[1], reinterpret_cast<const T*>(&row1)[1] } });
+    }
+
+    static CHTHOLLY_INLINE mat<2, 2, T> from_diag(const vec<2, T>& diag) noexcept
+    {
+        return mat<2, 2, T>({ { reinterpret_cast<const T*>(&diag)[0], zero<T> },
+                              { zero<T>, reinterpret_cast<const T*>(&diag)[1] } });
+    }
+};
 
 template<class Father, size_t Row, typename T>
 struct IMatMake<Father, mat<3, Row, T>> : Father
@@ -39,6 +54,23 @@ private:
     }
 };
 
+template<class Father, typename T>
+struct IMatMake<Father, mat<3, 3, T>> : Father
+{
+    static CHTHOLLY_INLINE mat<3, 3, T> from_row(const vec<3, T>& row0, const vec<3, T>& row1, const vec<3, T>& row2) noexcept
+    {
+        return mat<3, 3, T>({ { reinterpret_cast<const T*>(&row0)[0], reinterpret_cast<const T*>(&row1)[0], reinterpret_cast<const T*>(&row2)[0] },
+                              { reinterpret_cast<const T*>(&row0)[1], reinterpret_cast<const T*>(&row1)[1], reinterpret_cast<const T*>(&row2)[1] },
+                              { reinterpret_cast<const T*>(&row0)[2], reinterpret_cast<const T*>(&row1)[2], reinterpret_cast<const T*>(&row2)[2] } });
+    }
+
+    static CHTHOLLY_INLINE mat<3, 3, T> from_diag(const vec<3, T>& diag) noexcept
+    {
+        return mat<3, 3, T>({ { reinterpret_cast<const T*>(&diag)[0], zero<T>, zero<T> },
+                              { zero<T>, reinterpret_cast<const T*>(&diag)[1], zero<T> }, 
+                              { zero<T>, zero<T>, reinterpret_cast<const T*>(&diag)[2] } });
+    }
+};
 
 template<class Father, size_t Row, typename T>
 struct IMatMake<Father, mat<4, Row, T>> : Father
@@ -53,6 +85,26 @@ private:
     {
         return mat<4, Row, T>({ reinterpret_cast<const T*>(&row0)[Ns], reinterpret_cast<const T*>(&row1)[Ns],
                                 reinterpret_cast<const T*>(&row2)[Ns], reinterpret_cast<const T*>(&row3)[Ns] }...);
+    }
+};
+
+template<class Father, typename T>
+struct IMatMake<Father, mat<4, 4, T>> : Father
+{
+    static CHTHOLLY_INLINE mat<4, 4, T> from_row(const vec<4, T>& row0, const vec<4, T>& row1, const vec<4, T>& row2, const vec<4, T>& row3) noexcept
+    {
+        return mat<4, 4, T>({ { reinterpret_cast<const T*>(&row0)[0], reinterpret_cast<const T*>(&row1)[0], reinterpret_cast<const T*>(&row2)[0], reinterpret_cast<const T*>(&row3)[0] },
+                              { reinterpret_cast<const T*>(&row0)[1], reinterpret_cast<const T*>(&row1)[1], reinterpret_cast<const T*>(&row2)[1], reinterpret_cast<const T*>(&row3)[1] },
+                              { reinterpret_cast<const T*>(&row0)[2], reinterpret_cast<const T*>(&row1)[2], reinterpret_cast<const T*>(&row2)[2], reinterpret_cast<const T*>(&row3)[2] },
+                              { reinterpret_cast<const T*>(&row0)[3], reinterpret_cast<const T*>(&row1)[3], reinterpret_cast<const T*>(&row2)[3], reinterpret_cast<const T*>(&row3)[3] } });
+    }
+
+    static CHTHOLLY_INLINE mat<4, 4, T> from_diag(const vec<4, T>& diag) noexcept
+    {
+        return mat<4, 4, T>({ { reinterpret_cast<const T*>(&diag)[0], zero<T>, zero<T>, zero<T> },
+                              { zero<T>, reinterpret_cast<const T*>(&diag)[1], zero<T>, zero<T> }, 
+                              { zero<T>, zero<T>, reinterpret_cast<const T*>(&diag)[2], zero<T> },
+                              { zero<T>, zero<T>, zero<T>, reinterpret_cast<const T*>(&diag)[3] } });
     }
 };
 }
