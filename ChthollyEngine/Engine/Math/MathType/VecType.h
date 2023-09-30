58484e581c17008362846008e2bed4a7401467d0
YGXXD <20191120197@mail.ynu.edu.cn>
2023-09-30 21:40:36 +0800

    vectype template N

--- ChthollyEngine/Engine/Math/MathType/VecType.h
+++ ChthollyEngine/Engine/Math/MathType/VecType.h
@@ -10,35 +10,35 @@
 namespace ktm
 {
 
-template<int N, typename T>
+template<size_t N, typename T>
 struct vec<N, T> : ktl::SingleExtends_t<ktl::TemplateList<ktm::IArray, ktm::IVecData, ktm::IVecArray, ktm::IVecOpt>, vec<N, T>> 
 {
     using Father = ktl::SingleExtends_t<ktl::TemplateList<ktm::IArray, ktm::IVecData, ktm::IVecArray, ktm::IVecOpt>, vec<N, T>>; 
     using Father::Father;
 };
 
-template<int N>
+template<size_t N>
 using fvec = vec<N, float>;
 using fvec1 = fvec<1>;
 using fvec2 = fvec<2>;
 using fvec3 = fvec<3>;
 using fvec4 = fvec<4>;
 
-template<int N>
+template<size_t N>
 using svec = vec<N, int>;
 using svec1 = svec<1>;
 using svec2 = svec<2>;
 using svec3 = svec<3>;
 using svec4 = svec<4>;
 
-template<int N>
+template<size_t N>
 using uvec = vec<N, unsigned int>;
 using uvec1 = uvec<1>;
 using uvec2 = uvec<2>;
 using uvec3 = uvec<3>;
 using uvec4 = uvec<4>;
 
-template<int N>
+template<size_t N>
 using dvec = vec<N, double>;
 using dvec1 = dvec<1>;
 using dvec2 = dvec<2>;
