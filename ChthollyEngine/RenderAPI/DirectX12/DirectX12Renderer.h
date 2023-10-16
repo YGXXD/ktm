510227239d366707bd6e487fa6e2dd1b892577b4
chenqiudu <chenqiudu@kuaishou.com>
2023-10-16 10:14:18 +0800

    dx renderer

---
+++ ChthollyEngine/RenderAPI/DirectX12/DirectX12Renderer.h
@@ -0,0 +1,17 @@
+#ifndef _DIRECTX12_RENDERER_H_
+#define _DIRECTX12_RENDERER_H_
+
+#include "Render/Renderer.h"
+
+namespace keg
+{
+class DirectX12Renderer : public Renderer
+{
+public:
+
+private:
+
+};
+}
+
+#endif
