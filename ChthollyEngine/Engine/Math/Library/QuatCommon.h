c3f81d065fa50def7d7831ed5c63da73bc4eca64
chenqiudu <chenqiudu@kuaishou.com>
2023-12-11 20:45:43 +0800

    quat slerp

--- ChthollyEngine/Engine/Math/Library/QuatCommon.h
+++ ChthollyEngine/Engine/Math/Library/QuatCommon.h
@@ -50,6 +50,41 @@ CHTHOLLY_INLINE std::enable_if_t<is_quaternion_v<Q>, Q> normalize(const Q& q) no
     return Q(q.vector * rsqrt(length_squared));
 }
 
+// template<class Q>
+// CHTHOLLY_INLINE std::enable_if_t<is_quaternion_v<Q>, vec<3, quat_traits_base_t<Q>>> act(const Q& q, const vec<3, quat_traits_base_t<Q>>& v) noexcept
+// {
+//     using T = quat_traits_base_t<Q>;
+//     vec<3, quat_traits_base_t<Q>> t = static_cast<T>(2) * cross(q.imag(), v);
+//     return v + (q.real() * t) + cross(q.imag(), t);
+// }
+
+template<class Q>
+CHTHOLLY_INLINE std::enable_if_t<is_quaternion_v<Q>, Q> slerp_internal(const Q& x, const Q& y, quat_traits_base_t<Q> t)
+{
+    using T = quat_traits_base_t<Q>;
+    T s = one<T> - t;
+    T a = static_cast<T>(2) * atan2(length(x.vector - x.vector), length(x.vector + x.vector));// angel
+    T r = one<T> / sinc(a);
+    return normalize(Q(sinc(s * a) * r * s * x.vector + sinc(t * a) * r * t * y.vector));
+}
+
+template<class Q>
+CHTHOLLY_INLINE std::enable_if_t<is_quaternion_v<Q>, Q> slerp(const Q& x, const Q& y, quat_traits_base_t<Q> t)
+{
+    if (dot(x, y) >= 0)
+        return slerp_internal(x, y, t);
+    return slerp_internal(x, negate(y), t);
+}
+
+template<class Q>
+CHTHOLLY_INLINE std::enable_if_t<is_quaternion_v<Q>, Q> slerp_longest(const Q& x, const Q& y, quat_traits_base_t<Q> t)
+{
+    if (dot(x, y) >= 0)
+        return slerp_internal(x, negate(y), t);
+    return slerp_internal(x, y, t);
+}
+
+
 }
 
 #endif
