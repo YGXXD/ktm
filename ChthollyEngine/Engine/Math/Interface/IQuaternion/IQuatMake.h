cb1c07edca62c97004c6b1f2d67b43e755dddecb
chenqiudu <chenqiudu@kuaishou.com>
2023-12-13 20:28:52 +0800

    update vec left_mul mat

--- ChthollyEngine/Engine/Math/Interface/IQuaternion/IQuatMake.h
+++ ChthollyEngine/Engine/Math/Interface/IQuaternion/IQuatMake.h
@@ -4,6 +4,7 @@
 #include "Math/Library/ArtcBase.h"
 #include "Math/Library/ArtcCommon.h"
 #include "Math/Library/VecGeometry.h"
+#include "Math/Library/MatCommon.h"
 
 namespace ktm
 {
@@ -54,6 +55,44 @@ struct IQuatMake<Father, quat<T>> : Father
         return from_to_less_0p5pi(from, half) * from_to_less_0p5pi(half, to);
     }
 
+    static CHTHOLLY_INLINE quat<T> from_matrix(const mat<4, 4, T>& m) noexcept
+    {
+        const mat<3, 3, T>* m33_ptr = reinterpret_cast<const mat<3, 3, T>*>(&m);
+        return from_matrix(*m33_ptr);
+    }
+
+    static CHTHOLLY_NOINLINE quat<T> from_matrix(const mat<3, 3, T>& m) noexcept
+    {
+        T m_trace = trace(m);
+        if (m_trace >= zero<T>)
+        {
+            T r = static_cast<T>(2) * sqrt(one<T> + m_trace);
+            T rinv = one<T> / r;
+            return quat<T>(rinv * (m[1][2] - m[2][1]), rinv * (m[2][0] - m[0][2]), rinv * (m[0][1] - m[1][0]),
+                           r / static_cast<T>(4));
+        }
+        else if (m[0][0] >= m[1][1] && m[0][0] >= m[2][2])
+        {
+            T r = static_cast<T>(2) * sqrt(one<T> - m[1][1] - m[2][2] + m[0][0]);
+            T rinv = one<T> / r;
+            return quat<T>(r / static_cast<T>(4), rinv * (m[0][1] + m[1][0]), rinv * (m[0][2] + m[2][0]),
+                           rinv * (m[1][2] - m[2][1]));
+        }
+        else if (m[1][1] >= m[2][2])
+        {
+            T r = static_cast<T>(2) * sqrt(one<T> - m[0][0] - m[2][2] + m[1][1]);
+            T rinv = one<T> / r;
+            return quat<T>(rinv * (m[0][1] + m[1][0]), r / static_cast<T>(4), rinv * (m[1][2] + m[2][1]),
+                           rinv * (m[2][0] - m[0][2]));
+        }
+        else
+        {
+            T r = static_cast<T>(2) * sqrt(one<T> - m[0][0] - m[1][1] + m[2][2]);
+            T rinv = one<T> / r;
+            return quat<T>(rinv * (m[0][2] + m[2][0]), rinv * (m[1][2] + m[2][1]), r / static_cast<T>(4),
+                           rinv * (m[0][1] - m[1][0]));
+        }
+    }
 private:
     static CHTHOLLY_INLINE quat<T> from_to_less_0p5pi(const vec<3, T>& from, const vec<3, T>& to)
     {
--- ChthollyEngine/Engine/Math/Library/Transform3D.h
+++ ChthollyEngine/Engine/Math/Library/Transform3D.h
@@ -17,11 +17,10 @@ CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> view
     T wx = -dot(eye_pos, x);
     T wy = -dot(eye_pos, y);
     T wz = -dot(eye_pos, z);
-
-    return mat<4, 4, T>({ x[0], x[1], x[2], wx },
-                        { y[0] ,y[1], y[2], wy },
-                        { z[0], z[1], z[2], wz },
-                        { zero<T>, zero<T>, zero<T>, one<T> });
+    return mat<4, 4, T>({ x[0], y[0], z[0], zero<T> },
+                        { x[1] ,y[1], z[1], zero<T> },
+                        { x[2], y[2], z[2], zero<T> },
+                        { wx, wy, wz, one<T> });
 }
 
 template<typename T>
@@ -32,8 +31,8 @@ CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> pers
     T zs = zfar / ( znear - zfar );
     return mat<4, 4, T>({ xs, zero<T>, zero<T>, zero<T> },
                         { zero<T>, ys, zero<T>, zero<T> },
-                        { zero<T>, zero<T>, -zs, znear * zs },
-                        { zero<T>, zero<T>, one<T>, zero<T> });
+                        { zero<T>, zero<T>, -zs, one<T> },
+                        { zero<T>, zero<T>, znear * zs, zero<T> }); 
 }
 
 template<typename T>
@@ -42,35 +41,35 @@ CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> orth
     T dx = right - left;
     T dy = top - bottom;
     T dz = zfar - znear;
-    return mat<4, 4, T>({ static_cast<T>(2) / dx, zero<T>, zero<T>, (right + left) / (-dx) },
-                        { zero<T>, static_cast<T>(2) / dy, zero<T>, (top + bottom) / (-dy) },
-                        { zero<T>, zero<T>, one<T> / dz, znear / (-dz) },
-                        { zero<T>, zero<T>, zero<T>, one<T>});
+    return mat<4, 4, T>({ static_cast<T>(2) / dx, zero<T>, zero<T>, zero<T> },
+                        { zero<T>, static_cast<T>(2) / dy, zero<T>, zero<T> },
+                        { zero<T>, zero<T>, one<T> / dz, zero<T> },
+                        { (right + left) / (-dx), (top + bottom) / (-dy), znear / (-dz), one<T>});
 }
 
 template<typename T>
-CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_x(float angle_radians)
+CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_x(T angle_radians)
 {
     return mat<4, 4, T>({ one<T>, zero<T>, zero<T>, zero<T> },
-                        { zero<T>, cos(angle_radians), -sin(angle_radians), zero<T> },
-                        { zero<T>, sin(angle_radians), cos(angle_radians), zero<T> },
+                        { zero<T>, cos(angle_radians), sin(angle_radians), zero<T> },
+                        { zero<T>, -sin(angle_radians), cos(angle_radians), zero<T> },
                         { zero<T>, zero<T>, zero<T>, one<T> });
 }
 
 template<typename T>
-CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_y(float angle_radians)
+CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_y(T angle_radians)
 {
-    return mat<4, 4, T>({ cos(angle_radians), zero<T>, sin(angle_radians), zero<T> },
+    return mat<4, 4, T>({ cos(angle_radians), zero<T>, -sin(angle_radians), zero<T> },
                         { zero<T>, one<T>, zero<T>, zero<T> },
-                        { -sin(angle_radians), zero<T>, cos(angle_radians), zero<T> },
+                        { sin(angle_radians), zero<T>, cos(angle_radians), zero<T> },
                         { zero<T>, zero<T>, zero<T>, one<T> });
 }
 
 template<typename T>
-CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_z(float angle_radians)
+CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rotate_z(T angle_radians)
 {
-    return mat<4, 4, T>({ cos(angle_radians), -sin(angle_radians), zero<T>, zero<T> },
-                        { sin(angle_radians), cos(angle_radians), zero<T>, zero<T> },
+    return mat<4, 4, T>({ cos(angle_radians), sin(angle_radians), zero<T>, zero<T> },
+                        { -sin(angle_radians), cos(angle_radians), zero<T>, zero<T> },
                         { zero<T>, zero<T>, one<T>, zero<T> },
                         { zero<T>, zero<T>, zero<T>, one<T> });
 }
@@ -78,10 +77,10 @@ CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> rota
 template<typename T>
 CHTHOLLY_INLINE std::enable_if_t<std::is_floating_point_v<T>, mat<4, 4, T>> translate(const vec<3, T>& v)
 {
-    return mat<4, 4, T>({ one<T>, zero<T>, zero<T>, v[0] },
-                        { zero<T>, one<T>, zero<T>, v[1] },
-                        { zero<T>, zero<T>, one<T>, v[2] },
-                        { zero<T>, zero<T>, zero<T>, one<T> });
+    return mat<4, 4, T>({ one<T>, zero<T>, zero<T>, zero<T> },
+                        { zero<T>, one<T>, zero<T>, zero<T> },
+                        { zero<T>, zero<T>, one<T>, zero<T> },
+                        { v[0], v[1], v[2], one<T> });
 }
 
 template<typename T>
