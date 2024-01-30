7c7b9be40d781c52bbed1aa5c18dff8964e37542
chenqiudu <chenqiudu@kuaishou.com>
2024-01-30 18:05:05 +0800

    fix epsilon template typename

--- ktm/function/epsilon.h
+++ ktm/function/epsilon.h
@@ -46,37 +46,37 @@ KTM_INLINE std::enable_if_t<std::is_floating_point_v<T>, bool> greater(T x, T y,
     return greater_zero(x - y, e);
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> equal_zero(const V& x, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return reduce_max(abs(x)) < e;
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> equal(const V& x, const V& y, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return equal_zero(x - y, e);
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> less_zero(const V& x, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return reduce_max(x) <= -e;
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> less(const V& x, const V& y, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return less_zero(x - y, e);
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> greater_zero(const V& x, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return reduce_min(x) >= e;
 }
 
-template<typename V>
+template<class V>
 KTM_INLINE std::enable_if_t<is_vector_v<V> && is_floating_point_base_v<V>, bool> greater(const V& x, const V& y, vec_traits_base_t<V> e = std::numeric_limits<vec_traits_base_t<V>>::epsilon()) noexcept
 {
     return greater_zero(x - y, e);
