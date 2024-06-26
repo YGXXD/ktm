5d056c86f300efd9ce6a0472998baa7396ee3eeb
chenqiudu <chenqiudu@kuaishou.com>
2024-06-26 21:46:55 +0800

    update loop util

--- ktm/detail/loop_util.h
+++ ktm/detail/loop_util.h
@@ -23,16 +23,20 @@ KTM_FUNC void loop_op(R&& self, A1&& l0, OP&& op, std::index_sequence<Ns...>)
     ((self[Ns] = op(l0[Ns])), ...);
 }
 
+template<typename R, typename A1, typename OP>
+KTM_FUNC void loop_op(R&& self, A1&& l0, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self[i] = op(l0[i]);
+}
+
 template<size_t LoopN, typename R, typename A1, typename OP>
 KTM_FUNC void loop_op(R&& self, A1&& l0, OP&& op)
 {
     if constexpr(LoopN <= 4)
-        return loop_op(self, std::forward<A1>(l0), std::forward<OP>(op), std::make_index_sequence<LoopN>());
+        loop_op(self, std::forward<A1>(l0), std::forward<OP>(op), std::make_index_sequence<LoopN>());
     else
-    {
-        for(int i = 0; i < LoopN; ++i)
-            self[i] = op(l0[i]);
-    }
+        loop_op(self, std::forward<A1>(l0), std::forward<OP>(op), LoopN);
 }
 
 template<typename R, typename A1, typename A2, typename OP, size_t ...Ns>
@@ -41,16 +45,20 @@ KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, OP&& op, std::index_sequence<N
     ((self[Ns] = op(l0[Ns], l1[Ns])), ...);
 }
 
+template<typename R, typename A1, typename A2, typename OP>
+KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self[i] = op(l0[i], l1[i]);
+}
+
 template<size_t LoopN, typename R, typename A1, typename A2, typename OP>
 KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, OP&& op)
 {
     if constexpr(LoopN <= 4)
-        return loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<OP>(op), std::make_index_sequence<LoopN>());
+        loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<OP>(op), std::make_index_sequence<LoopN>());
     else
-    {
-        for(int i = 0; i < LoopN; ++i)
-            self[i] = op(l0[i], l1[i]);
-    }
+        loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<OP>(op), LoopN);
 }
 
 template<typename R, typename A1, typename A2, typename A3, typename OP, size_t ...Ns>
@@ -59,16 +67,20 @@ KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, A3&& l2, OP&& op, std::index_s
     ((self[Ns] = op(l0[Ns], l1[Ns], l2[Ns])), ...);
 }
 
+template<typename R, typename A1, typename A2, typename A3, typename OP>
+KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, A3&& l2, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self[i] = op(l0[i], l1[i], l2[i]);
+}
+
 template<size_t LoopN, typename R, typename A1, typename A2, typename A3, typename OP>
 KTM_FUNC void loop_op(R&& self, A1&& l0, A2&& l1, A3&& l2, OP&& op)
 {
     if constexpr(LoopN <= 4)
-        return loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<A3>(l2), std::forward<OP>(op), std::make_index_sequence<LoopN>());
+        loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<A3>(l2), std::forward<OP>(op), std::make_index_sequence<LoopN>());
     else
-    {
-        for(int i = 0; i < LoopN; ++i)
-            self[i] = op(l0[i], l1[i], l2[i]);
-    }
+        loop_op(self, std::forward<A1>(l0), std::forward<A2>(l1), std::forward<A3>(l2), std::forward<OP>(op), LoopN); 
 }
 
 template<typename R, typename A1, typename T, typename OP, size_t ...Ns>
@@ -77,16 +89,20 @@ KTM_FUNC void loop_scalar(R&& self, A1&& l0, const T& scalar, OP&& op, std::inde
     ((self[Ns] = op(l0[Ns], scalar)), ...);
 }
 
+template<typename R, typename A1, typename T, typename OP>
+KTM_FUNC void loop_scalar(R&& self, A1&& l0, const T& scalar, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self[i] = op(l0[i], scalar);
+}
+
 template<size_t LoopN, typename R, typename A1, typename T, typename OP>
 KTM_FUNC void loop_scalar(R&& self, A1&& l0, const T& scalar, OP&& op)
 {
     if constexpr(LoopN <= 4)
-        return loop_scalar(self, std::forward<A1>(l0), scalar, std::forward<OP>(op), std::make_index_sequence<LoopN>());
+        loop_scalar(self, std::forward<A1>(l0), scalar, std::forward<OP>(op), std::make_index_sequence<LoopN>());
     else
-    {
-        for(int i = 0; i < LoopN; ++i)
-            self[i] = op(l0[i], scalar);
-    }
+        loop_scalar(self, std::forward<A1>(l0), scalar, std::forward<OP>(op), LoopN);
 }
 
 template<typename R, typename A1, typename A2, typename T, typename OP, size_t ...Ns>
@@ -95,16 +111,20 @@ KTM_FUNC void loop_scalar(R&& self, A1&& l0, A2&& l1, const T& scalar, OP&& op, 
     ((self[Ns] = op(l0[Ns], l1[Ns], scalar)), ...);
 }
 
+template<typename R, typename A1, typename A2, typename T, typename OP>
+KTM_FUNC void loop_scalar(R&& self, A1&& l0, A2&& l1, const T& scalar, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self[i] = op(l0[i], l1[i], scalar);
+}
+
 template<size_t LoopN, typename R, typename A1, typename A2, typename T, typename OP>
 KTM_FUNC void loop_scalar(R&& self, A1&& l0, A2&& l1, const T& scalar, OP&& op)
 {
     if constexpr(LoopN <= 4)
-        return loop_scalar(self, std::forward<A1>(l0), std::forward<A2>(l1), scalar, std::forward<OP>(op), std::make_index_sequence<LoopN>());
+        loop_scalar(self, std::forward<A1>(l0), std::forward<A2>(l1), scalar, std::forward<OP>(op), std::make_index_sequence<LoopN>());
     else
-    {
-        for(int i = 0; i < LoopN; ++i)
-            self[i] = op(l0[i], l1[i], scalar);
-    }
+        loop_scalar(self, std::forward<A1>(l0), std::forward<A2>(l1), scalar, std::forward<OP>(op), LoopN);
 }
 
 template<typename T, typename A, typename OP, size_t ...Ns>
@@ -113,17 +133,21 @@ KTM_FUNC void loop_reduce(T& self, const A& l0, OP&& op, std::index_sequence<Ns.
     ((self = op(self, l0[Ns + 1])), ...);
 }
 
+template<typename T, typename A, typename OP>
+KTM_FUNC void loop_reduce(T& self, const A& l0, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self = op(self, l0[i + 1]);
+}
+
 template<size_t LoopN, typename T, typename A, typename OP>
 KTM_FUNC void loop_reduce(T& self, const A& l0, const T& init, OP&& op)
 {
     self = init;
     if constexpr(LoopN <= 4)
-        return loop_reduce(self, l0, std::forward<OP>(op), std::make_index_sequence<LoopN - 1>());
+        loop_reduce(self, l0, std::forward<OP>(op), std::make_index_sequence<LoopN - 1>());
     else
-    {
-        for(int i = 1; i < LoopN; ++i)
-            self = op(self, l0[i]);
-    }
+        loop_reduce(self, l0, std::forward<OP>(op), LoopN - 1); 
 }
 
 template<typename T, typename A, typename U, typename OP, size_t ...Ns>
@@ -132,17 +156,21 @@ KTM_FUNC void loop_reduce(T& self, const A& l0, const U& l1, OP&& op, std::index
     ((self = op(self, l0[Ns + 1], l1[Ns + 1])), ...);
 }
 
+template<typename T, typename A, typename U, typename OP>
+KTM_FUNC void loop_reduce(T& self, const A& l0, const U& l1, OP&& op, size_t loop)
+{
+    for(int i = 0; i < loop; ++i)
+        self = op(self, l0[i + 1], l1[i + 1]);
+}
+
 template<size_t LoopN, typename T, typename A, typename U, typename OP>
 KTM_FUNC void loop_reduce(T& self, const A& l0, const U& l1, const T& init, OP&& op)
 {
     self = init;
     if constexpr(LoopN <= 4)
-        return loop_reduce(self, l0, l1, std::forward<OP>(op), std::make_index_sequence<LoopN - 1>());
+        loop_reduce(self, l0, l1, std::forward<OP>(op), std::make_index_sequence<LoopN - 1>());
     else
-    {
-        for(int i = 1; i < LoopN; ++i)
-            self = op(self, l0[i], l1[i]);
-    }
+        loop_reduce(self, l0, l1, std::forward<OP>(op), LoopN - 1);
 }
 
 }
