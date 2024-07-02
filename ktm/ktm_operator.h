b140952cdb189bff1de2dc6d89cd913070dd047f
chenqiudu <chenqiudu@kuaishou.com>
2024-07-02 17:20:23 +0800

    fix msvc friend define compile

--- ktm/ktm_operator.h
+++ ktm/ktm_operator.h
@@ -17,4 +17,12 @@ KTM_FUNC constexpr T ktm_operator_madd(T x, T y, T z) noexcept { return x + y * 
 template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
 KTM_FUNC constexpr T ktm_operator_smadd(T& x, T y, T z) noexcept { x += y * z; return x; }
 
+namespace ktm
+{
+
+using ::ktm_operator_madd;
+using ::ktm_operator_smadd;
+
+}
+
 #endif
