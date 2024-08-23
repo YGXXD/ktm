3cd40e0b2b17d84cbff33dfd83e1f490e50baffc
chenqiudu <chenqiudu@kuaishou.com>
2024-08-23 18:54:46 +0800

    fix array io

--- ktm/interface/shared/iarray_io.h
+++ ktm/interface/shared/iarray_io.h
@@ -25,19 +25,9 @@ struct iarray_io : Father
     friend KTM_FUNC std::basic_ostream<T>& operator<<(std::basic_ostream<T>& out, const Child& x) noexcept { return x.std_stream_out(out); }
     template<typename T, typename = std::enable_if_t<std::is_exist_same_vs<char, wchar_t, T>>> 
     friend KTM_FUNC std::basic_istream<T>& operator>>(std::basic_istream<T>& in, Child& x) noexcept { return x.std_stream_in(in); }
-    
-    template<typename T, typename = std::enable_if_t<std::is_exist_same_vs<char, wchar_t, T>>>  
-    friend KTM_FUNC std::basic_ostringstream<T>& operator<<(std::basic_ostringstream<T>& out, const Child& x) noexcept { return x.std_stream_out(out); }
-    template<typename T, typename = std::enable_if_t<std::is_exist_same_vs<char, wchar_t, T>>> 
-    friend KTM_FUNC std::basic_istringstream<T>& operator>>(std::basic_istringstream<T>& in, Child& x) noexcept { return x.std_stream_in(in); }
-
-    template<typename T, typename = std::enable_if_t<std::is_exist_same_vs<char, wchar_t, T>>>  
-    friend KTM_FUNC std::basic_ofstream<T>& operator<<(std::basic_ofstream<T>& out, const Child& x) noexcept { return x.std_stream_out(out); }
-    template<typename T, typename = std::enable_if_t<std::is_exist_same_vs<char, wchar_t, T>>> 
-    friend KTM_FUNC std::basic_ifstream<T>& operator>>(std::basic_ifstream<T>& in, Child& x) noexcept { return x.std_stream_in(in); } 
 private:
-    template<template<typename ...> class OSTp, typename T>
-    KTM_FUNC OSTp<T>& std_stream_out(OSTp<T>& o) const noexcept
+    template<typename T>
+    KTM_FUNC std::basic_ostream<T>& std_stream_out(std::basic_ostream<T>& o) const noexcept
     {
         auto it = child_ptr()->begin();
         for(; it != child_ptr()->end() - 1; ++it)
@@ -46,8 +36,8 @@ private:
         return o;
     }
 
-    template<template<typename ...> class ISTp, typename T>
-    KTM_FUNC ISTp<T>& std_stream_in(ISTp<T>& i) noexcept
+    template<typename T>
+    KTM_FUNC std::basic_istream<T>& std_stream_in(std::basic_istream<T>& i) noexcept
     {
         for(auto it = child_ptr()->begin(); it != child_ptr()->end(); ++it)
             i >> *it;
