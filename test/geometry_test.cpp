247d63f142872d3619bf4441c13c11cbe9b1fc2b
chenqiudu <chenqiudu@kuaishou.com>
2025-01-16 16:11:20 +0800

    update test result

--- test/geometry_test.cpp
+++ test/geometry_test.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])
     TEST_EQUAL(ktm::length(ktm::fvec3(2.0f, 2.0f, 1.0f)), 3.0f);
     TEST_EQUAL(ktm::length(ktm::fvec4(2.0f, 2.0f, 2.0f, 2.0f)), 4.0f);
     TEST_EQUAL(ktm::length(ktm::fvec2(5.0f, 12.0f)), 13.0f);
-    TEST_EQUAL(ktm::length(ktm::fvec3(3.0f, 4.0f, 5.0f)), 7.071067811865475f);
-    TEST_EQUAL(ktm::length(ktm::fvec4(1.0f, 2.0f, 2.0f, 1.0f)), 3.1622776601683795f);
+    TEST_EQUAL(ktm::length(ktm::fvec3(3.0f, 4.0f, 5.0f)), 7.071068f);
+    TEST_EQUAL(ktm::length(ktm::fvec4(1.0f, 2.0f, 2.0f, 1.0f)), 3.162278f);
 
     TEST_EQUAL(ktm::distance(ktm::fvec2(0.0f, 0.0f), ktm::fvec2(3.0f, 4.0f)), 5.0f);
     TEST_EQUAL(ktm::distance(ktm::fvec2(-1.0f, -1.0f), ktm::fvec2(2.0f, 3.0f)), 5.0f);
@@ -33,9 +33,9 @@ int main(int argc, char* argv[])
     TEST_EQUAL(ktm::normalize(ktm::fvec2(3.0f, 4.0f)), ktm::fvec2(0.6f, 0.8f));
     TEST_EQUAL(ktm::normalize(ktm::fvec2(5.0f, 0.0f)), ktm::fvec2(1.0f, 0.0f));
     TEST_EQUAL(ktm::normalize(ktm::fvec2(0.0f, 5.0f)), ktm::fvec2(0.0f, 1.0f));
-    TEST_EQUAL(ktm::normalize(ktm::fvec2(1.0f, 1.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
-    TEST_EQUAL(ktm::normalize(ktm::fvec2(2.0f, 2.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
-    TEST_EQUAL(ktm::normalize(ktm::fvec2(3.0f, 3.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
+    TEST_EQUAL(ktm::normalize(ktm::fvec2(1.0f, 1.0f)), ktm::fvec2(0.707107f, 0.707107f));
+    TEST_EQUAL(ktm::normalize(ktm::fvec2(2.0f, 2.0f)), ktm::fvec2(0.707107f, 0.707107f));
+    TEST_EQUAL(ktm::normalize(ktm::fvec2(3.0f, 3.0f)), ktm::fvec2(0.707107f, 0.707107f));
 
     TEST_EQUAL(ktm::cross(ktm::fvec3(1.0f, 0.0f, 0.0f), ktm::fvec3(0.0f, 1.0f, 0.0f)), ktm::fvec3(0.0f, 0.0f, 1.0f));
     TEST_EQUAL(ktm::cross(ktm::fvec3(0.0f, 1.0f, 0.0f), ktm::fvec3(0.0f, 0.0f, 1.0f)), ktm::fvec3(1.0f, 0.0f, 0.0f));
@@ -58,11 +58,11 @@ int main(int argc, char* argv[])
     TEST_EQUAL(ktm::refract(ktm::fvec3(-1.0f, 1.0f, 0.0f), ktm::fvec3(1.0f, 0.0f, 0.0f), 0.7f),
                ktm::fvec3(-1.0f, 0.7f, 0.0f));
     TEST_EQUAL(ktm::refract(ktm::fvec3(2.0f, -2.0f, 0.0f), ktm::fvec3(0.0f, 1.0f, 0.0f), 0.8f),
-               ktm::fvec3(1.6f, -1.70880079f, 0.0f));
+               ktm::fvec3(1.6f, -1.708801f, 0.0f));
     TEST_EQUAL(ktm::refract(ktm::fvec3(2.0f, 0.0f, -2.0f), ktm::fvec3(0.0f, 0.0f, 1.0f), 0.9f),
-               ktm::fvec3(1.8f, 0.0f, -1.85202587f));
+               ktm::fvec3(1.8f, 0.0f, -1.852026f));
     TEST_EQUAL(ktm::refract(ktm::fvec3(-2.0f, 2.0f, 0.0f), ktm::fvec3(1.0f, 0.0f, 0.0f), 0.75f),
-               ktm::fvec3(-1.63935959f, 1.5f, 0.0f));
+               ktm::fvec3(-1.63936f, 1.5f, 0.0f));
 
     TEST_EQUAL_FAST(ktm::fast::length(ktm::fvec2(3.0f, 4.0f)), 5.0f);
     TEST_EQUAL_FAST(ktm::fast::length(ktm::fvec2(5.0f, 12.0f)), 13.0f);
@@ -81,9 +81,9 @@ int main(int argc, char* argv[])
     TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(3.0f, 4.0f)), ktm::fvec2(0.6f, 0.8f));
     TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(5.0f, 0.0f)), ktm::fvec2(1.0f, 0.0f));
     TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(0.0f, 5.0f)), ktm::fvec2(0.0f, 1.0f));
-    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(1.0f, 1.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
-    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(2.0f, 2.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
-    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(3.0f, 3.0f)), ktm::fvec2(0.7071067811865475f, 0.7071067811865475f));
+    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(1.0f, 1.0f)), ktm::fvec2(0.707107f, 0.707107f));
+    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(2.0f, 2.0f)), ktm::fvec2(0.707107f, 0.707107f));
+    TEST_EQUAL_FAST(ktm::fast::normalize(ktm::fvec2(3.0f, 3.0f)), ktm::fvec2(0.707107f, 0.707107f));
 
     TEST_EQUAL_FAST(ktm::fast::project(ktm::fvec3(1.0f, 0.0f, 0.0f), ktm::fvec3(2.0f, 0.0f, 0.0f)),
                     ktm::fvec3(1.0f, 0.0f, 0.0f));
